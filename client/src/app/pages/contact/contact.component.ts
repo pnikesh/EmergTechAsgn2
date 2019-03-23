e3ef0a689f808f52980461c7b02215ed819fd8d2
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-03-23 14:40:39 -0400

    added route guard for contact-list

--- client/src/app/pages/contact/contact.component.ts
+++ client/src/app/pages/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
 import { ActivatedRoute } from '@angular/router';
@@ -9,11 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContactComponent extends BasePageComponent implements OnInit {
 
-  constructor(route: ActivatedRoute) {
+  constructor(
+    route: ActivatedRoute,
+    private authService: AuthService
+    ) {
     super(route);
   }
 
   ngOnInit() {
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.loggedIn();
   }
 
 }
