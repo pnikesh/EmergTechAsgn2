af4e60e11beb311faea6c78136d2bdb0b8a8680c
Nik <nikeshpatel356@gmail.com>
2019-03-29 19:18:45 -0400

    Build for Huroku deployment

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   user: User;
   private authToken: any;
 
-  private endpoint = 'https://comp308-w2019-lesson10b.herokuapp.com/api/';
+  private endpoint = 'https://comp308-asgn2.herokuapp.com/api';
   //private endpoint = 'http://localhost:3000/api/';
 
   private httpOptions = {
